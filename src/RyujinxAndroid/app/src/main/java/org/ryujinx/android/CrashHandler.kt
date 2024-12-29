2c80e7a1eb277856ecb9d0fb8b514e053508556d
YGOMod <admir_zuzic@hotmail.com>
2024-12-29 20:30:36 +0100

    improved crashlog

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/CrashHandler.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/CrashHandler.kt
@@ -1,12 +1,18 @@
 package org.ryujinx.android
 
 import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 import java.lang.Thread.UncaughtExceptionHandler
 
 class CrashHandler : UncaughtExceptionHandler {
     var crashLog: String = ""
     override fun uncaughtException(t: Thread, e: Throwable) {
-        crashLog += e.toString() + "\n"
+        val sw = StringWriter()
+        e.printStackTrace(PrintWriter(sw))
+        val stackTrace = sw.toString()
+
+        crashLog += "Thread: ${t.name}\nException: ${e.toString()}\nStackTrace:\n$stackTrace\n"
 
         File(MainActivity.AppPath + "${File.separator}Logs${File.separator}crash.log").writeText(
             crashLog
